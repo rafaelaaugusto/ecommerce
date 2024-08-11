ee31ccc47a696528828e783ad9a1b3b764964b1a
rafaela <rafaela20augusto@gmail.com>
2024-08-11 17:15:02 -0300

    separate ProductSelectedItem

---
+++ frontend/lib/components/product_selected_tem_component.dart
@@ -0,0 +1,91 @@
+import 'package:brasil_fields/brasil_fields.dart';
+import 'package:fleasy/fleasy.dart';
+import 'package:flutter/material.dart';
+
+import '../functions/helper_functions.dart';
+import '../models/product_model.dart';
+import 'dialogs/remove_product_dialog.dart';
+import 'product_image_component.dart';
+
+class ProductSelectedItem extends StatelessWidget {
+  final ProductModel product;
+  final bool canRemoveItem;
+  final Function(ProductModel)? removeItem;
+
+  const ProductSelectedItem({
+    super.key,
+    required this.product,
+    this.canRemoveItem = true,
+    this.removeItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(
+        vertical: Insets.xl,
+        horizontal: Insets.m,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(Insets.m),
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.primaryContainer,
+        ),
+      ),
+      title: Text(
+        product.name,
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
+      subtitle: Row(
+        children: [
+          Text(
+            UtilBrasilFields.obterReal(
+              double.parse(product.price),
+              moeda: true,
+            ),
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge
+                ?.copyWith(fontSize: Insets.xl),
+          ),
+          const SizedBox(width: Insets.m),
+          if (product.hasDiscount && product.discountValue.isNotBlank)
+            Badge(
+              backgroundColor: Colors.red.shade200,
+              label: Text(
+                getPercentualValue(product.discountValue!),
+              ),
+            ),
+        ],
+      ),
+      leading: ProductImage(
+        itemId: product.id,
+      ),
+      trailing: canRemoveItem && removeItem != null
+          ? IconButton(
+              icon: const Icon(
+                Icons.close,
+              ),
+              color: Colors.red,
+              onPressed: () {
+                showAdaptiveDialog(
+                  context: context,
+                  builder: (context) => RemoveProductDialog(
+                    onPressed: () {
+                      removeItem?.call(product);
+                    },
+                  ),
+                );
+              },
+            )
+          : null,
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          '/product-details',
+          arguments: product,
+        );
+      },
+    );
+  }
+}
