e79fb206bbc3f6d5590e9ad0e40034c1221c9071
rafaela <rafaela20augusto@gmail.com>
2024-08-12 02:33:10 -0300

    remove console.log

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -3,8 +3,6 @@ const Order = require("../models/order");
 const createOrder = async (req, res) => {
     try {
       const { id, user, products, total } = req.body;
-      console.log(req.body);
-  
   
       const newOrder = new Order({
         id,
@@ -12,11 +10,8 @@ const createOrder = async (req, res) => {
         products,
         total,
       });
-
-      console.log(newOrder);
   
       const savedOrder = await newOrder.save();
-      console.log(savedOrder);
       res.status(201).json(savedOrder);
     } catch (error) {
       res.status(500).json({ message: 'Failed to create order', error });
