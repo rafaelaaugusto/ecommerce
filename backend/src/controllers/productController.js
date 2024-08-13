fa45d29d52741b8b0158396e4d6e82f6952b41ac
rafaela <rafaela20augusto@gmail.com>
2024-08-12 23:15:25 -0300

    add status

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -3,7 +3,7 @@ const productService = require('../services/productService');
 const getAllProducts = async (req, res) => {
     try {
       const products = await productService.getAllProducts();
-      res.json(products);
+      res.status(200).json(products);
     } catch (error) {
       res.status(500).json({ message: 'Failed to fetch products' });
     }
