20a1272c9f69936570a273689efc77085a939d13
rafaela <rafaela20augusto@gmail.com>
2024-08-13 13:11:34 -0300

    add onWillPop

--- frontend/lib/views/order_details_view.dart
+++ frontend/lib/views/order_details_view.dart
@@ -10,36 +10,53 @@ class OrderDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final OrderModel order =
-        ModalRoute.of(context)?.settings.arguments as OrderModel;
+    final Map<String, dynamic> params =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
+    final order = params['order'] as OrderModel;
+    final isFromShoppingCart = params['isFromShoppingCart'] as bool? ?? false;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detalhes da compra'),
-      ),
-      body: OrderDetails(
-        orderId: order.idFormatted,
-        user: order.user,
-        products: order.products,
-        createdAt: order.createdAt,
-      ),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.only(
-          bottom: Insets.xxl,
-          left: Insets.l * 2,
-          right: Insets.l * 2,
+    void onWillPop() {
+      if (isFromShoppingCart) {
+        Navigator.popAndPushNamed(context, '/');
+      } else {
+        Navigator.pop(context);
+      }
+    }
+
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        if (didPop) return;
+        onWillPop();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Detalhes da compra'),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              'Total: ${UtilBrasilFields.obterReal(
-                double.parse(order.total),
-                moeda: true,
-              )}',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ],
+        body: OrderDetails(
+          orderId: order.idFormatted,
+          user: order.user,
+          products: order.products,
+          createdAt: order.createdAt,
+        ),
+        bottomSheet: Padding(
+          padding: const EdgeInsets.only(
+            bottom: Insets.xxl,
+            left: Insets.l * 2,
+            right: Insets.l * 2,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Total: ${UtilBrasilFields.obterReal(
+                  double.parse(order.total),
+                  moeda: true,
+                )}',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ],
+          ),
         ),
       ),
     );
