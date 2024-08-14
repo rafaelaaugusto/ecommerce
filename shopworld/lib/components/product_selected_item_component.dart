d4a0c312ba53c6d82e63da838931bb97c53d7c58
rafaela <rafaela20augusto@gmail.com>
2024-08-14 18:52:52 -0300

    refactor component

--- shopworld/lib/components/product_selected_item_component.dart
+++ shopworld/lib/components/product_selected_item_component.dart
@@ -21,82 +21,95 @@ class ProductSelectedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: Insets.m,
-          horizontal: Insets.xl,
-        ),
-        title: Text(
-          product.name,
-          style: Theme.of(context).textTheme.titleSmall,
-        ),
-        subtitle: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  UtilBrasilFields.obterReal(
-                    double.parse(product.price),
-                    moeda: true,
-                  ),
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleLarge
-                      ?.copyWith(fontSize: Insets.xl),
-                ),
-                const SizedBox(width: Insets.m),
-                if (product.hasDiscount && product.discountValue.isNotBlank)
-                  Badge(
-                    backgroundColor: Colors.red.shade200,
-                    label: Text(
-                      getPercentualValue(product.discountValue!),
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          '/product-details',
+          arguments: product,
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(Insets.xl),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ProductImage(
+                heigth: '80',
+                width: '80',
+                itemId: product.id,
+              ),
+              const SizedBox(
+                width: Insets.xl,
+              ),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.name,
+                      style: Theme.of(context).textTheme.titleSmall,
                     ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          UtilBrasilFields.obterReal(
+                            double.parse(product.price),
+                            moeda: true,
+                          ),
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge
+                              ?.copyWith(fontSize: Insets.xl),
+                        ),
+                        const SizedBox(width: Insets.m),
+                        if (product.hasDiscount &&
+                            product.discountValue.isNotBlank)
+                          Badge(
+                            backgroundColor: Colors.red.shade200,
+                            label: Text(
+                              getPercentualValue(product.discountValue!),
+                            ),
+                          ),
+                      ],
+                    ),
+                    Text(
+                      product.description ?? '',
+                      style: Theme.of(context).textTheme.labelSmall,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              if (canRemoveItem && removeItem != null) ...[
+                const SizedBox(width: Insets.l),
+                IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    size: 18,
                   ),
+                  color: Colors.red,
+                  onPressed: () {
+                    showAdaptiveDialog(
+                      context: context,
+                      builder: (context) => ActionAlertDialog(
+                        title: 'Remover produto',
+                        subtitle:
+                            'Tem certeza que deseja remover este produto?',
+                        onPressed: () {
+                          removeItem?.call(product);
+                        },
+                      ),
+                    );
+                  },
+                ),
               ],
-            ),
-            Text(
-              product.description ?? '',
-              style: Theme.of(context).textTheme.labelSmall,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
+            ],
+          ),
         ),
-        leading: ProductImage(
-          itemId: product.id,
-        ),
-        trailing: canRemoveItem && removeItem != null
-            ? IconButton(
-                icon: const Icon(
-                  Icons.close,
-                  size: 18,
-                ),
-                color: Colors.red,
-                onPressed: () {
-                  showAdaptiveDialog(
-                    context: context,
-                    builder: (context) => ActionAlertDialog(
-                      title: 'Remover produto',
-                      subtitle: 'Tem certeza que deseja remover este produto?',
-                      onPressed: () {
-                        removeItem?.call(product);
-                      },
-                    ),
-                  );
-                },
-              )
-            : null,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(Insets.m),
-        ),
-        onTap: () {
-          Navigator.pushNamed(
-            context,
-            '/product-details',
-            arguments: product,
-          );
-        },
       ),
     );
   }
